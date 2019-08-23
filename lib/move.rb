fa37fb9e32e5631cd1c489c92bd390dfb91ffca9
Nicholas Ratti <nickratti@gmail.com>
2019-08-23 11:13:18 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,12 +6,5 @@ def display_board( board) #board is the argument that this method will return - 
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index( user_input) #this creates a method called input_to_index and defines user_input as the one argument
-  value = user_input.to_i-1 #this line sets a varaible called "value" equal to the user_input argument and then converts it to an integer and subtracts one
-end
-
-def move(array, index, value = "X") #this creates a method called move that accepts 3 arguments 1) the array being edited, 2) the location within the index, 3) the new value of that will replace the previous value within the array, and sets a default value of "X" if no other value is given
-  array[index] = value
-end
 
 # code your input_to_index and move method here!
